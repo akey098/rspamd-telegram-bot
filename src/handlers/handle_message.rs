9598c6a897f53b5b14a793813ec1f0436107a969
akey098 <ae12240@auca.kg>
2025-05-30 22:55:41 +0500

    Added logic to handle permanent and timely ban

--- src/handlers/handle_message.rs
+++ src/handlers/handle_message.rs
@@ -1,8 +1,10 @@
+use crate::config::{field, key, symbol};
 use crate::handlers::scan_msg;
+use chrono::{Duration, Utc};
 use redis::Commands;
 use std::error::Error;
 use teloxide::prelude::*;
-use crate::config::{field, key, symbol};
+use teloxide::types::ChatPermissions;
 
 pub async fn handle_message(
     bot: Bot,
@@ -32,15 +34,70 @@ pub async fn handle_message(
             .hget(key.clone(), field::ADMIN_CHAT)
             .expect("Failed to get admin chat");
     }
-    if scan_result.score >= 10.0
+    if scan_result.symbols.contains_key(symbol::TG_PERM_BAN) {
+        let _ = bot.delete_message(chat_id, message.id).await;
+        let _ = bot.ban_chat_member(chat_id, user_id).await;
+        println!("User {} in chat {} banned", user_id, chat_id);
+        if admin_chat_exists {
+            bot.send_message(
+                ChatId(admin_chat[0]),
+                format!(
+                    "Banned user {} in chat {} for spam",
+                    user_id, chat_id
+                ),
+            )
+                .await?;
+        } else {
+            bot.send_message(
+                chat_id,
+                format!(
+                    "Banned user {} in chat {} for spam",
+                    user_id, chat_id
+                ),
+            )
+                .await?;
+        }
+    }
+    else if scan_result.symbols.contains_key(symbol::TG_BAN) {
+        let _ = bot.delete_message(chat_id, message.id).await;
+        println!(
+            "Deleting message {} from chat {} because it appears to be spam.",
+            message.id, chat_id
+        );
+        let ttl: i64 = redis_conn
+            .httl(key.clone(), field::BANNED)
+            .expect("Failed to check user's banned.");
+        let _ = mute_user_for(bot.clone(), chat_id, user_id, ttl).await;
+        println!("Muted user {} for {}", user_id, ttl);
+        if admin_chat_exists {
+            bot.send_message(
+                ChatId(admin_chat[0]),
+                format!(
+                    "Deleted message {} from user {} in chat {}. Muted user {} for {}",
+                    message.id, user_id, chat_id, user_id, ttl
+                ),
+            )
+                .await?;
+        } else {
+            bot.send_message(
+                chat_id,
+                format!(
+                    "Deleted message {} from user {} in chat {}. Muted user {} for {}",
+                    message.id, user_id, chat_id, user_id, ttl
+                ),
+            )
+                .await?;
+        }
+        
+    } else if scan_result.score >= 10.0
         || scan_result.symbols.contains_key(symbol::TG_FLOOD)
         || scan_result.symbols.contains_key(symbol::TG_SUSPICIOUS)
     {
         println!(
             "Deleting message {} from chat {} because it appears to be spam.",
-            message.id, message.chat.id
+            message.id, chat_id
         );
-        bot.delete_message(message.chat.id, message.id).await?;
+        bot.delete_message(chat_id, message.id).await?;
         let _: () = redis_conn
             .hincr(key.clone(), field::DELETED, 1)
             .expect("Failed to update deleted count");
@@ -49,16 +106,16 @@ pub async fn handle_message(
                 ChatId(admin_chat[0]),
                 format!(
                     "Deleting message {} from user {} in chat {} for spam.",
-                    message.id, user_id, message.chat.id
+                    message.id, user_id, chat_id
                 ),
             )
             .await?;
         } else {
             bot.send_message(
-                message.chat.id,
+                chat_id,
                 format!(
                     "Deleting message {} from user {} in chat {} for spam.",
-                    message.id, user_id, message.chat.id
+                    message.id, user_id, chat_id
                 ),
             )
             .await?;
@@ -66,23 +123,23 @@ pub async fn handle_message(
     } else if scan_result.score >= 5.0 {
         println!(
             "Warning user {} in chat {} about spammy behavior.",
-            user_id, message.chat.id
+            user_id, chat_id
         );
         if admin_chat_exists {
             bot.send_message(
                 ChatId(admin_chat[0]),
                 format!(
                     "Warning message {} from user {} in chat {} is looks like spam.",
-                    message.id, user_id, message.chat.id
+                    message.id, user_id, chat_id
                 ),
             )
             .await?;
         } else {
             bot.send_message(
-                message.chat.id,
+                chat_id,
                 format!(
                     "Warning message {} from user {} in chat {} is looks like spam.",
-                    message.id, user_id, message.chat.id
+                    message.id, user_id, chat_id
                 ),
             )
             .await?;
@@ -98,5 +155,22 @@ pub async fn handle_message(
     }
     
     
+    Ok(())
+}
+
+async fn mute_user_for(
+    bot: Bot,
+    chat_id: ChatId,
+    user_id: UserId,
+    seconds: i64,
+) -> anyhow::Result<()> {
+    let until_ts = Utc::now() + Duration::seconds(seconds);
+
+    let perms = ChatPermissions::empty();
+
+    bot.restrict_chat_member(chat_id, user_id, perms)
+        .until_date(until_ts)
+        .await?;
+
     Ok(())
 }
