4b5d9fe3dd96602eab5a64c3e74d9553b9e215ff
akey098 <ae12240@auca.kg>
2025-06-02 03:15:16 +0500

    MVP white/blacklist

--- rspamd-config/lua.local.d/whiteblacklist.lua
+++ rspamd-config/lua.local.d/whiteblacklist.lua
@@ -1,25 +1,36 @@
 local rspamd_redis = require "rspamd_redis"
 
 local settings = {
-
+    whitelist_key = 'tg:whitelist:',
+    blacklist_key = 'tg:blacklist:'
 }
 
 rspamd_config:register_symbol('WHITELIST', 1.0, function(task)
     local user_id = tostring(task:get_header('X-Telegram-User', true) or "")
-    local chat_id = tostring(task:get_header('X-Telegram-Chat', true) or "")
     if user_id == "" then return false end
     local user_key = settings.user_prefix .. user_id
-
-
+    local function whitelist_cb (err, data)
+        if err or not data then return end
+        if data then
+            task:insert_result('WHITELIST', 1.0)
+        end
+    end
+    rspamd_redis.make_request({task=task, host="127.0.0.1:6379",
+                               cmd='SISMEMBER', args={settings.whitelist_key, user_key}, callback=whitelist_cb})
 end)
 
 rspamd_config:register_symbol('BLACKLIST', 1.0, function(task)
     local user_id = tostring(task:get_header('X-Telegram-User', true) or "")
-    local chat_id = tostring(task:get_header('X-Telegram-Chat', true) or "")
     if user_id == "" then return false end
     local user_key = settings.user_prefix .. user_id
-
-
+    local function whitelist_cb (err, data)
+        if err or not data then return end
+        if data then
+            task:insert_result('WHITELIST', 1.0)
+        end
+    end
+    rspamd_redis.make_request({task=task, host="127.0.0.1:6379",
+                               cmd='SISMEMBER', args={settings.blacklist_key, user_key}, callback=whitelist_cb})
 end)
 
 rspamd_config:set_metric_symbol('WHITELIST', -20.0, 'User is in whitelist')
