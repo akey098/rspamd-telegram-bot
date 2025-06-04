561d5de26d6377dcd2f5b10eef280bdb88c95e37
akey098 <ae12240@auca.kg>
2025-06-04 09:06:07 +0500

    Add white/black list for the words

--- rspamd-config/lua.local.d/whiteblacklist.lua
+++ rspamd-config/lua.local.d/whiteblacklist.lua
@@ -1,37 +1,53 @@
 local rspamd_redis = require "rspamd_redis"
 
 local settings = {
-    whitelist_key = 'tg:whitelist:',
-    blacklist_key = 'tg:blacklist:'
+    whitelist_users_key = 'tg:whitelist:users',
+    blacklist_users_key = 'tg:blacklist:users',
+    whitelist_words_key = 'tg:whitelist:words',
+    blacklist_words_key = 'tg:blacklist:words'
 }
 
-rspamd_config:register_symbol('WHITELIST', 1.0, function(task)
+rspamd_config:register_symbol('WHITELIST_USER', 1.0, function(task)
     local user_id = tostring(task:get_header('X-Telegram-User', true) or "")
     if user_id == "" then return false end
     local user_key = settings.user_prefix .. user_id
     local function whitelist_cb (err, data)
         if err or not data then return end
         if data then
-            task:insert_result('WHITELIST', 1.0)
+            task:insert_result('WHITELIST_USER', 1.0)
         end
     end
     rspamd_redis.make_request({task=task, host="127.0.0.1:6379",
-                               cmd='SISMEMBER', args={settings.whitelist_key, user_key}, callback=whitelist_cb})
+                               cmd='SISMEMBER', args={settings.whitelist_users_key, user_key}, callback=whitelist_cb})
 end)
 
-rspamd_config:register_symbol('BLACKLIST', 1.0, function(task)
+rspamd_config:register_symbol('BLACKLIST_USER', 1.0, function(task)
     local user_id = tostring(task:get_header('X-Telegram-User', true) or "")
     if user_id == "" then return false end
     local user_key = settings.user_prefix .. user_id
     local function whitelist_cb (err, data)
         if err or not data then return end
         if data then
-            task:insert_result('WHITELIST', 1.0)
+            task:insert_result('BLACKLIST_USER', 1.0)
         end
     end
     rspamd_redis.make_request({task=task, host="127.0.0.1:6379",
-                               cmd='SISMEMBER', args={settings.blacklist_key, user_key}, callback=whitelist_cb})
+                               cmd='SISMEMBER', args={settings.blacklist_users_key, user_key}, callback=whitelist_cb})
 end)
 
-rspamd_config:set_metric_symbol('WHITELIST', -20.0, 'User is in whitelist')
-rspamd_config:set_metric_symbol('WHITELIST', 20.0, 'User is in blacklist')
+rspamd_config:register_symbol('WHITELIST_WORD', 1.0, function(task)
+    local user_id = tostring(task:get_header('X-Telegram-User', true) or "")
+    if user_id == "" then return false end
+
+end)
+
+rspamd_config:register_symbol('BLACKLIST_WORD', 1.0, function(task)
+    local user_id = tostring(task:get_header('X-Telegram-User', true) or "")
+    if user_id == "" then return false end
+
+end)
+
+rspamd_config:set_metric_symbol('WHITELIST_USER', -20.0, 'User is in whitelist')
+rspamd_config:set_metric_symbol('BLACKLIST_USER', 20.0, 'User is in blacklist')
+rspamd_config:set_metric_symbol('WHITELIST_WORD', -2.0, 'Word is in whitelist')
+rspamd_config:set_metric_symbol('BLACKLIST_WORD', 2.0, 'Word is in blacklist')
