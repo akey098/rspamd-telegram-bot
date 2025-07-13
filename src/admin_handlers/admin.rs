9da2bc6726a7610852026a14c273c05a2aaf32d7
akey098 <ae12240@auca.kg>
2025-07-13 13:50:00 +0500

    Remove unused FuzzyAdd command from admin command handler for cleaner code

--- src/admin_handlers/admin.rs
+++ src/admin_handlers/admin.rs
@@ -510,9 +510,7 @@ pub async fn handle_admin_command(bot: Bot, msg: Message, cmd: AdminCommand) -> 
                     }
                 }
             }
-            AdminCommand::FuzzyAdd => {
-                handle_fuzzy_add(&bot, msg.clone()).await?;
-            }
+
         }
     } else {
         bot.send_message(chat_id, "You are not admin").await?;
